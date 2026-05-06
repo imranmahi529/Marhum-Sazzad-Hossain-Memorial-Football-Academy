12eccca7dd669f541c2d22d43f53913f25efacf6
MSH FOOTBALL ACADEMY <imahi6791@gmail.com>
2026-05-06 22:41:17 +0600

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,26 @@ function downloadPDF() {
         pagebreak: { mode: ['avoid-all'] }
     }).from(element).save();
 }
+function downloadPDF() {
+    const element = document.getElementById("formArea");
+
+    html2pdf().set({
+        margin: 5,
+        filename: 'MSH_Registration.pdf',
+        image: { type: 'jpeg', quality: 1 },
+
+        html2canvas: {
+            scale: 2,
+            useCORS: true,
+            scrollY: 0
+        },
+
+        jsPDF: {
+            unit: 'mm',
+            format: 'a4',
+            orientation: 'portrait'
+        },
+
+        pagebreak: { mode: ['avoid-all'] }
+    }).from(element).save();
+}
